2c3a435451ce220e21c27e8f973c7866ddf99cf7
ZhilPro27 <zhilalkrisna@gmail.com>
2025-03-14 10:23:15 +0700

    Membuat header dan deklarasi variable global

---
+++ file2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];    // Deklarasi variable global array dengan panjang 20 dan nama arr
+int n;  // Deklarasi variable global n untuk menyimpan elemen pada array
