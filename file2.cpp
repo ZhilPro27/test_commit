54bf20f5f6df544032b6f6f5320c06f2a4d18a6b
ZhilPro27 <zhilalkrisna@gmail.com>
2025-03-14 10:24:13 +0700

    membuat prosedur bubbleSortArray

--- file2.cpp
+++ file2.cpp
@@ -26,3 +26,20 @@ void input(){   // Prosedur input
         cin >> arr[i];
     }
 } 
+
+void bubbleSortArray(){ // BEGIN
+    int pass = 1; // step 1
+    do
+    { // BEGIN
+        for(int j=0;j<=n-1-pass;j++){ // step 2
+            if (arr[j]>arr[j+1]){ // step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+        pass = pass + 1; // step 4
+    } while (pass <= n-1); // step 5
+    
+} // END
