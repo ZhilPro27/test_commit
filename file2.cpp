7196ef5cd39090c0bd5b023179c2874cdd6e061a
ZhilPro27 <zhilalkrisna@gmail.com>
2025-03-14 10:24:33 +0700

    Membuat prosedur display

--- file2.cpp
+++ file2.cpp
@@ -43,3 +43,18 @@ void bubbleSortArray(){ // BEGIN
     } while (pass <= n-1); // step 5
     
 } // END
+
+
+void display(){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    cout << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1) {
+            cout << " --> ";
+        }
+    }
+}
